d7bdc087998170a8665e89f39f3599292caf3655
Veselin <veselqshko@gmail.com>
2015-09-10 15:26:46 +0200

    Localization System - XML files for each language, scripts, demo scene, and UI prefab

---
+++ Minigame1/Assets/Core/GUIManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class GUIManager : MonoBehaviour
+{
+    void Awake()
+    {
+		/*
+        LanguageManager.Instance.LoadLanguage("English");
+        Debug.Log(LanguageManager.Instance.Get("Phrases/Hello"));
+
+        LanguageManager.Instance.LoadLanguage("Swedish");
+        Debug.Log(LanguageManager.Instance.Get("Phrases/Hello"));
+		*/
+		if(PlayerPrefs.GetString ("Language") != null)
+		{
+			LanguageManager.Instance.LoadLanguage(PlayerPrefs.GetString ("Language"));
+		}
+		else
+		{
+			PlayerPrefs.SetString("Language", "English");
+			LanguageManager.Instance.LoadLanguage(PlayerPrefs.GetString ("Language"));
+		}
+    }
+
+	public void OnStartGameClick()
+	{
+		Debug.Log ("Start the gaddeim game");
+		//Application.LoadLevel ("Level01");
+	}
+
+    public void OnLanguageClick(string language)
+    {
+        LanguageManager.Instance.LoadLanguage(language);
+		PlayerPrefs.SetString ("Language", language);
+        LocalizedText[] texts = FindObjectsOfType<LocalizedText>();
+        foreach (LocalizedText text in texts)
+        {
+            text.LocalizeText();
+        }
+    }
+}
