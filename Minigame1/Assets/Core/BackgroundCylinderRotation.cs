b93636838ed23ce503db3cb8fa737232bbd81987
Tomas Gislason Moesgaard <tomahawk@os.dk>
2015-09-10 14:48:55 +0200

    Cylinder rotation script added

---
+++ Minigame1/Assets/Core/BackgroundCylinderRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundCylinderRotation : MonoBehaviour {
+
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(Vector3.right * Time.deltaTime * speed);
+
+
+    }
+}
