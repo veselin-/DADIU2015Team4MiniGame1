25494741e3d606543a86a33948dc73ce06d15aca
Tomas Gislason Moesgaard <tomahawk@os.dk>
2015-09-10 14:24:47 +0200

    Touch movement implemented and fixed so there is no possibility of swiping.

--- Minigame1/Assets/Core/BGControl.cs
+++ Minigame1/Assets/Core/BGControl.cs
@@ -7,48 +7,47 @@ public class BGControl : MonoBehaviour {
 
     public float speed = 5f;
 
+    float destination = 0f;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
-	
-	}
-
-    void OnMouseDrag()
-    {
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit = new RaycastHit();
-
-
-        if (Physics.Raycast(ray, out hit, 100, 8))
-        {
-
-
-
-        }
-            
-
-        Debug.Log(Input.mousePosition);
-
-        if (Input.mousePosition.x >= Elephant.transform.position.x)
+        if (destination >= Elephant.transform.position.x)
         {
 
             Elephant.transform.Translate(Vector3.right * Time.deltaTime * speed);
 
         }
 
-        if (Input.mousePosition.x < Elephant.transform.position.x)
+        if (destination < Elephant.transform.position.x)
         {
 
             Elephant.transform.Translate(Vector3.left * Time.deltaTime * speed);
 
         }
 
+
+    }
+
+    void OnMouseDown()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+
+            destination = hit.point.x;
+
+        }
+            
     }
 
 }
