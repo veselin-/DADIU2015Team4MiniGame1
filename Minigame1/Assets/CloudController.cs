ec5066f8cc791d39620806543278be698f9eecfe
Tomas Gislason Moesgaard <tomahawk@os.dk>
2015-09-14 12:14:58 +0200

    Further implementation of clouds

---
+++ Minigame1/Assets/CloudController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudController : MonoBehaviour {
+
+    public GameObject[] clouds;
+
+    public Texture2D[] textures;
+
+    public GameObject[] spawns;
+ 
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SpawnCloud(GameObject cloud)
+    {
+
+    }
+
+
+}
---
+++ Minigame1/Assets/CloudMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMovement : MonoBehaviour {
+
+    public float speed = 1f;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(Vector3.back * Time.deltaTime * speed);
+
+    }
+}
