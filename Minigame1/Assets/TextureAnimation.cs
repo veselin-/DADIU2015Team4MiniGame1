d2edcba494a1292f1690e4a93d2b5584a3df9156
Tomas Gislason Moesgaard <tomahawk@os.dk>
2015-09-15 10:04:00 +0200

    Added gfx for the dark cloud

---
+++ Minigame1/Assets/TextureAnimation.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureAnimation : MonoBehaviour {
+
+    public Texture2D tex1, tex2;
+
+    public float animationTime = 0;
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        animationTime += Time.deltaTime;
+
+        if(animationTime < 3f)
+        {
+            noThunder();
+        }
+        else if(animationTime < 3.5f)
+        {
+            thunder();
+        }
+        else if(animationTime < 4f)
+        {
+            noThunder();
+        }
+        else if(animationTime < 5f)
+        {
+            thunder();
+        }
+        else if(animationTime >= 5f)
+        {
+            animationTime = 0f;
+        }
+
+	
+	}
+
+    void thunder()
+    {
+        GetComponent<Renderer>().material.SetTexture("_MainTex", tex2);
+    }
+
+    void noThunder()
+    {
+        GetComponent<Renderer>().material.SetTexture("_MainTex", tex1);
+    }
+}
+
+
